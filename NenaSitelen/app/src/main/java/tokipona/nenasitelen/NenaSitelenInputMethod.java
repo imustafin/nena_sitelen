c030524e411e020766d052fe4946ea1cef815ea3
Ilgiz Mustafin <ilgimustafin@gmail.com>
2016-12-14 22:21:01 +0300

    Copy keyboard from https://code.tutsplus.com/tutorials/create-a-custom-keyboard-on-android--cms-22615

---
+++ NenaSitelen/app/src/main/java/tokipona/nenasitelen/NenaSitelenInputMethod.java
@@ -0,0 +1,109 @@
+package tokipona.nenasitelen;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.media.AudioManager;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+/**
+ * Created by ilgiz on 12/14/16.
+ */
+
+public class NenaSitelenInputMethod extends InputMethodService
+        implements KeyboardView.OnKeyboardActionListener {
+
+
+    private KeyboardView mKv;
+    private Keyboard mKeyboard;
+
+    private boolean mCaps = false;
+
+    @Override
+    public View onCreateInputView() {
+        mKv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
+        mKeyboard = new Keyboard(this, R.xml.qwerty);
+        mKv.setKeyboard(mKeyboard);
+        mKv.setOnKeyboardActionListener(this);
+        return mKv;
+    }
+
+    private void playClick(int keyCode){
+        AudioManager am = (AudioManager)getSystemService(AUDIO_SERVICE);
+        switch(keyCode){
+            case 32:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+                break;
+            case Keyboard.KEYCODE_DONE:
+            case 10:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case Keyboard.KEYCODE_DELETE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+                break;
+            default: am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+        }
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+        playClick(primaryCode);
+        switch(primaryCode){
+            case Keyboard.KEYCODE_DELETE :
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                mCaps = !mCaps;
+                mKeyboard.setShifted(mCaps);
+                mKv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char)primaryCode;
+                if(Character.isLetter(code) && mCaps){
+                    code = Character.toUpperCase(code);
+                }
+                ic.commitText(String.valueOf(code),1);
+        }
+    }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
